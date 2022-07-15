fc9201ccf61fe50c4c28b687bdf34a0113c45506
Burgos <nandobfer@yahoo.com.br>
2022-07-15 09:27:39 -0300

    added home page and css

--- server.py
+++ server.py
@@ -2,10 +2,17 @@ from flask import Flask, request, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # redirecting to another endpoint
     return redirect(url_for('home'))
+
+
+@app.route('/home/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
