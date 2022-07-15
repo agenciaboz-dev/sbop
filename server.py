5bc200276d651ddd50e73c2e989fee61e8f75c8a
Burgos <nandobfer@yahoo.com.br>
2022-07-15 09:17:54 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, url_for, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # redirecting to another endpoint
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="5000")
