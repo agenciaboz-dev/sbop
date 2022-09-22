361250c3f5be20f6c756cbb88fb756825f5c0ea6
nandobfer <nandobfer@yahoo.com.br>
2022-09-22 15:17:22 -0300

    fix: replace all uppercase column names with lowercase

--- src/session_handler.py
+++ src/session_handler.py
@@ -204,7 +204,7 @@ class Session():
             especialidades = 'especialidades_str'
 
         try:
-            sql = f"UPDATE Membros SET NOME='{data['name']}', UF='{data['uf']}', CEP='{data['cep']}', CPF='{data['cpf']}', EMAIL='{data['email']}', CRM='{data['crm']}', CURRICULUM='{data['curriculum']}', TELEFONE='{data[telefone]}', ENDERECO='{data['endereco']}', NUMERO='{data['numero']}', COMPLEMENTO='{data['complemento']}', BAIRRO='{data['bairro']}', CIDADE='{data['cidade']}', ESPECIALIDADES='{data[especialidades]}', TEMPORARIO='{data['temporario']}' WHERE ID={data['id']}"
+            sql = f"UPDATE Membros SET nome='{data['name']}', uf='{data['uf']}', cep='{data['cep']}', cpf='{data['cpf']}', email='{data['email']}', crm='{data['crm']}', curriculum='{data['curriculum']}', telefone='{data[telefone]}', endereco='{data['endereco']}', numero='{data['numero']}', complemento='{data['complemento']}', bairro='{data['bairro']}', cidade='{data['cidade']}', especialidades='{data[especialidades]}', temporario='{data['temporario']}' WHERE id={data['id']}"
             print(sql)
             cursor = self.database.connection.cursor()
             cursor.execute(sql)
