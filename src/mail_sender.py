d2d472e09f1431d58aa3d8147e87e9f58b532b5f
Burgos <nandobfer@yahoo.com.br>
2022-09-26 08:11:49 -0300

    update: changed mail lib

--- src/mail_sender.py
+++ src/mail_sender.py
@@ -1,23 +1,17 @@
 # from src.config import email as config
-# import yagmail
+from redmail import EmailSender
 
-# def sendMail(destination, subject, message):
-#     print(config['login'], config['password'])
-#     yag = yagmail.SMTP(config['login'], config['password'])
-#     yag.send(destination, subject, message)
+email = EmailSender(
+    host="mail.sbop.com.br",
+    port=25,
+    user_name="noreply@sbop.com.br",
+    password="oht#yoYNO^R2"
+)
 
-import smtplib
-
-#Ports 465 and 587 are intended for email client to email server communication - sending email
-server = smtplib.SMTP('mail.sbop.com.br', 25)
-
-#starttls() is a way to take an existing insecure connection and upgrade it to a secure connection using SSL/TLS.
-server.starttls()
-
-#Next, log in to the server
-server.login("noreply@sbop.com.br", "oht#yoYNO^R2")
-
-msg = "TESTE"
-
-#Send the mail
-server.sendmail("noreply@sbop.com.br", "fernando@agenciazop.com.br", msg)
+email.send(
+    sender="noreply@sbop.com.br",
+    receivers=["fernando@agenciazop.com.br"],
+    subject="An example email",
+    text="Hi, this is text body.",
+    html="<h1>Hi,</h1><p>this is HTML body</p>"
+)
