bbfb48a75e0ae1a9ba5ea590eb208318fd3d91f1
nandobfer <nandobfer@yahoo.com.br>
2022-10-17 16:39:21 -0300

    update: added map svg mobile

---
+++ static/javascript/map.js
@@ -0,0 +1,10 @@
+const map = $('.map-container svg');
+const width = $(window).width();
+
+$('document').ready(() => {
+    map.css('transform', `scale(${width/1100})`);
+    
+    if (width < 640) {
+        map.css('transform', `scale(${width/600})`);
+    }
+})
