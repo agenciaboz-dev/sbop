edcf70161aac6e2301ea6d4e0aa8d1c939b5b319
Burgos <nandobfer@yahoo.com.br>
2022-10-18 08:28:53 -0300

    test: email template

--- src/mail_templates.py
+++ src/mail_templates.py
@@ -1,5 +1,8 @@
 def recoverPasswordTemplate(nome, link):
     return """
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
