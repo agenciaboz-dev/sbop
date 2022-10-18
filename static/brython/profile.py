39f263d29d0fe3434dd0a13817f39f0d35bab567
Burgos <nandobfer@yahoo.com.br>
2022-10-18 10:14:59 -0300

    teste: member login

--- static/brython/profile.py
+++ static/brython/profile.py
@@ -627,7 +627,7 @@ def loadMember():
 def preLoad(req):
     global member
 
-    data = eval(req.text)
+    data = eval(req.text.replace('null', 'None'))
     member = Member(data)
 
     if member.type:
