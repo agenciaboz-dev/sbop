d418e6f80bfb62c1419c1efc3333991c5fe75ddd
nandobfer <nandobfer@yahoo.com.br>
2022-12-07 09:55:24 -0300

    feat: added certificate script

--- src/certificate.py
+++ src/certificate.py
@@ -2,10 +2,25 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+# copyfile("newPDF.pdf", "basePDF.pdf")
 
 packet = io.BytesIO()
 can = canvas.Canvas(packet, pagesize=letter)
-can.drawString(10, 100, "Hello world")
+
+# drawing member name
+can.setFillColorRGB(0.86328, 0.8, 0.496)
+pdfmetrics.registerFont(TTFont('Montserrat', 'static/fonts/Montserrat-Regular.ttf'))
+can.setFont("Montserrat", 30)
+can.drawCentredString(420, 220, "Marcos Testador")
+
+# drawing member membership
+can.setFillColorRGB(0.082, 0.38281, 0.58593)
+pdfmetrics.registerFont(TTFont('Montserrat-SemiBold', 'static/fonts/Montserrat-SemiBold.ttf'))
+can.setFont("Montserrat-SemiBold", 12)
+can.drawCentredString(362, 181, "Associado".upper())
 can.save()
 
 #move to the beginning of the StringIO buffer
@@ -14,13 +29,13 @@ packet.seek(0)
 # create a new PDF with Reportlab
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
-existing_pdf = PdfFileReader(open("original.pdf", "rb"))
+existing_pdf = PdfFileReader(open("src/template.pdf", "rb"))
 output = PdfFileWriter()
 # add the "watermark" (which is the new pdf) on the existing page
 page = existing_pdf.getPage(0)
 page.mergePage(new_pdf.getPage(0))
 output.addPage(page)
 # finally, write "output" to a real file
-outputStream = open("destination.pdf", "wb")
+outputStream = open("static/documents/certificate.pdf", "wb")
 output.write(outputStream)
 outputStream.close()
